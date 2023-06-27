ea19a69315891f5173737e618fc87b51d2ee06c9
amenesca <alanmenescal@hotmail.com>
2023-06-27 10:15:01 -0300

    get_colors_textures.c diminuida

--- sources/read_get_validate/get_colors_textures.c
+++ sources/read_get_validate/get_colors_textures.c
@@ -1,5 +1,47 @@
 #include "../../includes/cub3d.h"
 
+int get_no_so(t_map *map, int i)
+{
+		if (ft_strnstr(map->file_read[i], "NO ",\
+ ft_strlen(map->file_read[i])) != NULL)
+		{
+			if (map->textures[NO] == NULL)
+				map->textures[NO] = ft_strdup(map->file_read[i]);
+			else
+				return (-1);
+		}
+		if (ft_strnstr(map->file_read[i], "SO ",\
+ ft_strlen(map->file_read[i])) != NULL)
+		{
+			if (map->textures[SO] == NULL)
+				map->textures[SO] = ft_strdup(map->file_read[i]);
+			else
+				return (-1);
+		}
+		return (0);
+}
+
+int get_we_ea(t_map *map, int i)
+{
+		if (ft_strnstr(map->file_read[i], "WE ",\
+ ft_strlen(map->file_read[i])) != NULL)
+		{
+			if (map->textures[WE] == NULL)
+				map->textures[WE] = ft_strdup(map->file_read[i]);
+			else
+				return (-1);
+		}
+		if (ft_strnstr(map->file_read[i], "EA ",\
+ ft_strlen(map->file_read[i])) != NULL)
+		{
+			if (map->textures[EA] == NULL)
+				map->textures[EA] = ft_strdup(map->file_read[i]);
+			else
+				return (-1);
+		}
+		return (0);
+}
+
 static int	 get_textures(t_map *map)
 {
 	int i;
@@ -7,34 +49,10 @@ static int	 get_textures(t_map *map)
 	i = -1;
 	while (map->file_read[++i] != NULL)
 	{
-		if (ft_strnstr(map->file_read[i], "NO ", ft_strlen(map->file_read[i])) != NULL)
-		{
-			if (map->textures[NO] == NULL)
-				map->textures[NO] = ft_strdup(map->file_read[i]);
-			else
-				return (-1);
-		}
-		else if (ft_strnstr(map->file_read[i], "SO ", ft_strlen(map->file_read[i])) != NULL)
-		{
-			if (map->textures[SO] == NULL)
-				map->textures[SO] = ft_strdup(map->file_read[i]);
-			else
-				return (-1);
-		}
-		else if (ft_strnstr(map->file_read[i], "WE ", ft_strlen(map->file_read[i])) != NULL)
-		{
-			if (map->textures[WE] == NULL)
-				map->textures[WE] = ft_strdup(map->file_read[i]);
-			else
-				return (-1);
-		}
-		else if (ft_strnstr(map->file_read[i], "EA ", ft_strlen(map->file_read[i])) != NULL)
-		{
-			if (map->textures[EA] == NULL)
-				map->textures[EA] = ft_strdup(map->file_read[i]);
-			else
-				return (-1);
-		}
+		if (get_no_so(map, i) != 0)
+			return (-1);
+		if (get_we_ea(map, i) != 0)
+			return (-1);
 	}
 	map->textures[4] = NULL;
 	return (0);
@@ -47,15 +65,16 @@ static int	get_colors(t_map *map)
 	i = -1;
 	while (map->file_read[++i] != NULL)
 	{
-		if (ft_strnstr(map->file_read[i], "F ", ft_strlen(map->file_read[i])) != NULL)
+		if (ft_strnstr(map->file_read[i], "F ",\
+ ft_strlen(map->file_read[i])) != NULL)
 		{
 			if (map->colors[0] == NULL)
 				map->colors[0] = ft_strdup(map->file_read[i]);
 			else
 				return (-1);
-
 		}
-		else if (ft_strnstr(map->file_read[i], "C ", ft_strlen(map->file_read[i])) != NULL)
+		else if (ft_strnstr(map->file_read[i], "C ",\
+ ft_strlen(map->file_read[i])) != NULL)
 		{
 			if (map->colors[1] == NULL)
 				map->colors[1] = ft_strdup(map->file_read[i]);

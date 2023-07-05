a9a2669e0038fdb7b2953c62f0faecef2d327f31
amenesca <alanmenescal@hotmail.com>
2023-07-05 16:09:11 -0300

    erro bobo resolvido

--- sources/init_mlx/xpm_to_img.c
+++ sources/init_mlx/xpm_to_img.c
@@ -15,7 +15,7 @@
 void	ft_xpm_to_img(t_img *imgtex, t_map *map, t_mlx *mlx, int pos)
 {
 	imgtex->img = mlx_xpm_file_to_image(mlx->mlx, \
-	map->textures[pos], &imgtex->tex_width, &imgtex->tex_width);
+	map->textures[pos], &imgtex->tex_width, &imgtex->tex_height);
 	imgtex->addr = NULL;
 	imgtex->addr = mlx_get_data_addr(imgtex->img, \
 	&imgtex->bits, &imgtex->line, &imgtex->endian);
